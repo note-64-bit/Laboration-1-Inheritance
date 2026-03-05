47a3703d621c7257afa8d14e08f918d233997eca
aarushsingh <aarushs@chalmers.se>
2026-03-05 14:47:26 +0100

    labb 4

---
+++ WorldConfig.java
@@ -0,0 +1,12 @@
+import java.awt.Rectangle;
+
+public final class WorldConfig {
+    private WorldConfig(){}
+
+    public static final int CAR_W = 100;
+    public static final int CAR_H = 60;
+
+    public static final Rectangle VOLVO_WORKSHOP_BOUNDS = new Rectangle(0, 465, 80, 80);
+
+    public static final int MAX_CARS = 10;
+}
